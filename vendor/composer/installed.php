28601a0c2d6bd363c6ebafc217cbb34c4ebb0dfe
shayaansiddiqui <shayaan.siddiqui@gmail.com>
2025-05-05 22:53:26 -0400

    PHP version

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'notbadtech/notbadtech',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '778ea6118f88ac4532c1bd360ef7d4f5119c8c90',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'notbadtech/notbadtech' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '778ea6118f88ac4532c1bd360ef7d4f5119c8c90',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
